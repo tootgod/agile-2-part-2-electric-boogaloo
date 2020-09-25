5e06a5a1a337ed8b459e6873f7d1c02714ffd51e
Zakurath <63682172+Zakurath@users.noreply.github.com>
2020-09-24 17:22:05 -0700

    base game

---
+++ Intermediate/Build/Win64/Building_EscapeEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/OneDrive/Documents/Unreal Projects/building escape/Intermediate/Build/Win64/Building_EscapeEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/Building_Escape/Building_EscapeGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BUILDING_ESCAPE_Building_EscapeGameModeBase_generated_h
+#error "Building_EscapeGameModeBase.generated.h already included, missing '#pragma once' in Building_EscapeGameModeBase.h"
+#endif
+#define BUILDING_ESCAPE_Building_EscapeGameModeBase_generated_h
+
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_RPC_WRAPPERS
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABuilding_EscapeGameModeBase(); \
+	friend struct Z_Construct_UClass_ABuilding_EscapeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABuilding_EscapeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/Building_Escape"), NO_API) \
+	DECLARE_SERIALIZER(ABuilding_EscapeGameModeBase)
+
+
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABuilding_EscapeGameModeBase(); \
+	friend struct Z_Construct_UClass_ABuilding_EscapeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABuilding_EscapeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/Building_Escape"), NO_API) \
+	DECLARE_SERIALIZER(ABuilding_EscapeGameModeBase)
+
+
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABuilding_EscapeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABuilding_EscapeGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABuilding_EscapeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABuilding_EscapeGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABuilding_EscapeGameModeBase(ABuilding_EscapeGameModeBase&&); \
+	NO_API ABuilding_EscapeGameModeBase(const ABuilding_EscapeGameModeBase&); \
+public:
+
+
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABuilding_EscapeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABuilding_EscapeGameModeBase(ABuilding_EscapeGameModeBase&&); \
+	NO_API ABuilding_EscapeGameModeBase(const ABuilding_EscapeGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABuilding_EscapeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABuilding_EscapeGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABuilding_EscapeGameModeBase)
+
+
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_12_PROLOG
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_RPC_WRAPPERS \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_INCLASS \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BUILDING_ESCAPE_API UClass* StaticClass<class ABuilding_EscapeGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID building_escape_Source_Building_Escape_Building_EscapeGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
