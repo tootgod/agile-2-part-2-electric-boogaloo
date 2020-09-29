ef7dfb55c711cdbb93b9a2115bb27559d219002b
Kyler Geesink <32684761+tootgod@users.noreply.github.com>
2020-09-28 22:15:43 -0700

    Adds Open Door Class

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/Building_Escape/Grabber.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BUILDING_ESCAPE_Grabber_generated_h
+#error "Grabber.generated.h already included, missing '#pragma once' in Grabber.h"
+#endif
+#define BUILDING_ESCAPE_Grabber_generated_h
+
+#define agile2_Source_Building_Escape_Grabber_h_14_RPC_WRAPPERS
+#define agile2_Source_Building_Escape_Grabber_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define agile2_Source_Building_Escape_Grabber_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGrabber(); \
+	friend struct Z_Construct_UClass_UGrabber_Statics; \
+public: \
+	DECLARE_CLASS(UGrabber, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Building_Escape"), NO_API) \
+	DECLARE_SERIALIZER(UGrabber)
+
+
+#define agile2_Source_Building_Escape_Grabber_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesUGrabber(); \
+	friend struct Z_Construct_UClass_UGrabber_Statics; \
+public: \
+	DECLARE_CLASS(UGrabber, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Building_Escape"), NO_API) \
+	DECLARE_SERIALIZER(UGrabber)
+
+
+#define agile2_Source_Building_Escape_Grabber_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UGrabber(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGrabber) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGrabber); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGrabber); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGrabber(UGrabber&&); \
+	NO_API UGrabber(const UGrabber&); \
+public:
+
+
+#define agile2_Source_Building_Escape_Grabber_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGrabber(UGrabber&&); \
+	NO_API UGrabber(const UGrabber&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGrabber); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGrabber); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UGrabber)
+
+
+#define agile2_Source_Building_Escape_Grabber_h_14_PRIVATE_PROPERTY_OFFSET
+#define agile2_Source_Building_Escape_Grabber_h_11_PROLOG
+#define agile2_Source_Building_Escape_Grabber_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	agile2_Source_Building_Escape_Grabber_h_14_PRIVATE_PROPERTY_OFFSET \
+	agile2_Source_Building_Escape_Grabber_h_14_RPC_WRAPPERS \
+	agile2_Source_Building_Escape_Grabber_h_14_INCLASS \
+	agile2_Source_Building_Escape_Grabber_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define agile2_Source_Building_Escape_Grabber_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	agile2_Source_Building_Escape_Grabber_h_14_PRIVATE_PROPERTY_OFFSET \
+	agile2_Source_Building_Escape_Grabber_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	agile2_Source_Building_Escape_Grabber_h_14_INCLASS_NO_PURE_DECLS \
+	agile2_Source_Building_Escape_Grabber_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BUILDING_ESCAPE_API UClass* StaticClass<class UGrabber>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID agile2_Source_Building_Escape_Grabber_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
